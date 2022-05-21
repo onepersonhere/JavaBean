87b8092c8f1f26837fcd06ee26ca21a093af557d
onepersonhere <51043502+onepersonhere@users.noreply.github.com>
2022-05-21 10:04:09 +0800

    Added stackable items

--- Inventory/JSONData.gd
+++ Inventory/JSONData.gd
@@ -1,14 +1,14 @@
 extends Node
 
-var item_data: Dictionary
+var item_data : Dictionary
 
 func _ready():
-	item_data = LoadData("res://Data/ItemData.json")
+	item_data = LoadData("res://Inventory/Data/ItemData.json")
 
 func LoadData(file_path):
 	var json_data
 	var file_data = File.new()
-	
+
 	file_data.open(file_path, File.READ)
 	json_data = JSON.parse(file_data.get_as_text())
 	file_data.close()
