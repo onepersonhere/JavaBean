a43f67ad93bd1f2a712807ce78e88d3524898e34
Ng Shi Jun <shijunng2010@gmail.com>
2022-06-18 17:56:50 +0800

    enemy AI

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,12 +1,51 @@
 extends KinematicBody2D
 
 const DeathEffect = preload("res://Effects/BatDeathEffect.tscn")
+
+export var ACCELERATION = 500
+export var MAX_SPEED = 80
+export var FRICTION = 150
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
+var state = IDLE
+
 onready var stats = $Stats
+onready var sprite = $AnimatedSprite
+onready var playerDetectionZone = $PlayerDetectionZone
 
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+			
+		WANDER:
+			pass
+			
+		CHASE:
+			var player = playerDetectionZone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			else:
+				state = IDLE
+			sprite.flip_h = velocity.x < 0
+			
+	velocity = move_and_slide(velocity)
+		
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
 
 func _on_Bat_HurtBox_area_entered(area):
 	knockback = area.knockback_vector * 150
