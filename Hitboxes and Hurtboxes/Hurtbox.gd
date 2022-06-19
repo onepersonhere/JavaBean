33e46b839c998d134ec57d6a99d371b74dcf3ce2
onepersonhere <51043502+onepersonhere@users.noreply.github.com>
2022-06-19 09:14:30 +0800

    bug fixes

--- Hitboxes and Hurtboxes/Hurtbox.gd
+++ Hitboxes and Hurtboxes/Hurtbox.gd
@@ -7,7 +7,7 @@ const HitEffect = preload("res://Effects/HitEffect.tscn")
 func _on_Hurtbox_area_entered(area):
 	if show_hit:
 		var effect = HitEffect.instance()
-		var main  = get_tree().current_scene
+		var main  = get_node("/root/World")
 		main.add_child(effect)
 		effect.global_position = global_position
 
