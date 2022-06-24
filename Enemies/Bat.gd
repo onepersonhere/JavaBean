b568c9548aa2e980b3daba224c0af4495a3f5663
Ng Shi Jun <shijunng2010@gmail.com>
2022-06-24 21:39:31 +0800

    bat wander state

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -24,9 +24,11 @@ onready var stats = $Stats
 onready var sprite = $AnimatedSprite
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var softCollision = $SoftCollision
+onready var wanderController = $WanderController
 
 func _ready():
 	timer.one_shot = true
+	state = pick_random_state([IDLE, WANDER])
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -36,14 +38,28 @@ func _physics_process(delta):
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
+			if wanderController.get_time_left() == 0:
+				state = pick_random_state([IDLE, WANDER])
+				wanderController.start_wander_timer(rand_range(1, 2))
 			
 		WANDER:
-			pass
+			seek_player()
+			if wanderController.get_time_left() == 0:
+				state = pick_random_state([IDLE, WANDER])
+				wanderController.start_wander_timer(rand_range(1, 2))
+			
+			var direction = global_position.direction_to(wanderController.target_position)
+			velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			sprite.flip_h = velocity.x < 0
+			
+			if global_position.distance_to(wanderController.target_position) <= 4:
+				state = pick_random_state([IDLE, WANDER])
+				wanderController.start_wander_timer(rand_range(1, 2))
 			
 		CHASE:
 			var player = playerDetectionZone.player
 			if player != null:
-				var direction = (player.global_position - global_position).normalized()
+				var direction = global_position.direction_to(player.global_position)
 				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
 			else:
 				state = IDLE
@@ -57,6 +73,10 @@ func _physics_process(delta):
 func seek_player():
 	if playerDetectionZone.can_see_player():
 		state = CHASE
+		
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
 
 func _on_Bat_HurtBox_area_entered(area):
 	get_node("/root/World").combat_start()
