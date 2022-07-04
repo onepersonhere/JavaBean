2b886bc959b671d4b09256f525b9c000f7b34da5
onepersonhere <51043502+onepersonhere@users.noreply.github.com>
2022-07-04 11:09:38 +0800

    Finished inventory side of the shop

--- Characters/Actions/ItemPanel.gd
+++ Characters/Actions/ItemPanel.gd
@@ -3,6 +3,8 @@ extends Panel
 onready var player = get_tree().get_nodes_in_group("Player")[0]
 onready var price = $Price/Price_val.text
 onready var amt = $Amt/Amt_val.text
+var location;
+var stuff;
 
 func _ready():
 	check_empty()
@@ -11,16 +13,19 @@ func _on_Sell_1_pressed():
 	player.COINS += int(price);
 	amt = str(int(amt) - 1);
 	check_empty()
+	update_inventory()
 
 func _on_Sell_Half_pressed():
 	player.COINS += int(price) * (int(amt) / 2);
 	amt = str(int(amt)/2);
 	check_empty()
+	update_inventory()
 
 func _on_Sell_All_pressed():
 	player.COINS += int(price) * int(amt);
 	amt = str(0);
 	check_empty()
+	update_inventory()
 
 func check_empty():
 	if int(amt) < 2:
@@ -35,12 +40,49 @@ func update_vals():
 	get_tree().get_nodes_in_group("ShopAction")[0].update_val()
 	player.update_stat_vals();
 	
-	update_inventory();
 	add_to_shop();
 
 func update_inventory():
-	# TODO
-	pass
+	if int(amt) == 0:
+		match location:
+			"inventory":
+				PlayerInventory.inventory.erase(stuff)
+			"hotbar":
+				PlayerInventory.hotbar.erase(stuff)
+	else:
+		match location:
+			"inventory":
+				PlayerInventory.inventory[stuff][1] = amt
+			"hotbar":
+				PlayerInventory.hotbar[stuff][1] = amt
+				
+	var parent = player.get_node("UI").get_node("CanvasLayer").get_node("UserInterface")
+	var old_inventory = get_tree().get_nodes_in_group("Inventory")[0]
+	var old_hotbar = get_tree().get_nodes_in_group("Hotbar")[0]
+	
+	var inventory = load("res://Inventory/Inventory.tscn").instance();
+	var hotbar = load("res://Inventory/Hotbar.tscn").instance();
+	# properties to match
+	var name = old_inventory.get_name()
+	old_inventory.set_name("old_inventory")
+	inventory.set_name(name)
+	
+	name = old_hotbar.get_name()
+	old_hotbar.set_name("old_hotbar")
+	hotbar.set_name(name)
+	
+	inventory.set_position(old_inventory.get_position())
+	hotbar.set_position(old_hotbar.get_position())
+	
+	inventory.visible = old_inventory.visible
+	hotbar.set_scale(old_hotbar.get_scale())
+	
+	old_inventory.queue_free()
+	old_hotbar.queue_free()
+	
+	parent.add_child(inventory, true);
+	parent.add_child(hotbar, true);
+		
 	
 func add_to_shop():
 	# TODO
--- Characters/Actions/ShopAction.gd
+++ Characters/Actions/ShopAction.gd
@@ -15,6 +15,7 @@ func interact():
 		
 		update_val()
 		load_inventory_stuff()
+		load_shop_stuff()
 	else:
 		$Popup.hide()
 		emit_signal("finished")
@@ -31,21 +32,35 @@ func load_inventory_stuff():
 	var hotbar = PlayerInventory.hotbar
 	
 	for stuff in inventory:
-		add_as_panel(stuff, inventory)
+		if not check_if_exist(stuff, "inventory"):
+			add_as_panel(stuff, inventory, "inventory")
 		
 	for stuff in hotbar:
-		add_as_panel(stuff, hotbar)
+		if not check_if_exist(stuff, "hotbar"):
+			add_as_panel(stuff, hotbar, "hotbar")
 	
 
-func add_as_panel(stuff, location):
+func add_as_panel(stuff, location, location_name):
 	var item_name = location[stuff][0]
 	var amt = location[stuff][1]
 	var panel = load("res://Characters/Actions/ItemPanel.tscn").instance();
 	panel.find_node("TextureRect").texture = load("res://Inventory/Icons/" + item_name + ".png")
 	panel.find_node("Name_val").text = item_name
 	panel.find_node("Amt_val").text = str(amt)
+	
+	panel.location = location_name
+	panel.stuff = stuff
 	# TODO: Construct a pricing system based on weightage of the price of the object.
 	panel.find_node("Price_val").text = str(rng.randi_range(1, 100)) + " coin(s)"
 	inventory_stuff.add_child(panel)
 
+func check_if_exist(cstuff, clocation):
+	var children = inventory_stuff.get_children()
+	for child in children:
+		if child.stuff == cstuff && child.location == clocation:
+			return true
+	return false
+
 # TODO: Complete the buy and sell script.
+func load_shop_stuff():
+	pass
