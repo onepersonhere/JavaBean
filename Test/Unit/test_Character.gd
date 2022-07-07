2f1ee4f1e6cf141fa8247bb2e1df0cd857f8e6cf
onepersonhere <51043502+onepersonhere@users.noreply.github.com>
2022-07-07 20:09:39 +0800

    Fixed some of the tests

--- Test/Unit/test_Character.gd
+++ Test/Unit/test_Character.gd
@@ -2,25 +2,34 @@ extends 'res://addons/gut/test.gd'
 
 var world = load("res://World/World.tscn")
 var _world = null
-var player = null
+var player = load("res://Characters/MainCharacter.tscn")
+var _player = null
+var UI = load("res://UI/UI.tscn")
+var _UI = null
 
 func before_each():
 	_world = world.instance()
+	_UI = UI.instance()
+	_player = player.instance()
+	get_node("/root").add_child(_UI)
 	get_node("/root").add_child(_world)
-	player = get_node("/root/World/YSort/Player")
-
+	get_node("/root/World/YSort").add_child(_player)
+	
 func after_each():
 	_world.queue_free()
-
+	_UI.queue_free()
+	
 func test_position():
-	assert_true(player.position.x == 1936 and player.position.y == 968)
+	_player.set_position(Vector2(1936, 968))
+	assert_true(_player.position.x == 1936 and _player.position.y == 968)
 
 func test_item_position():
+	_player.set_position(Vector2(1936, 968))
 	var item_drop = load("res://Inventory/Items/ItemDrop.tscn").instance()
 	get_node("/root/World/YSort").add_child(item_drop)
 	
-	item_drop.position.x = player.position.x
-	item_drop.position.y = player.position.y
+	item_drop.position.x = _player.position.x
+	item_drop.position.y = _player.position.y
 	
 	assert_true(item_drop.position.x == 1936 and item_drop.position.y == 968)
 	item_drop.queue_free()
@@ -35,7 +44,7 @@ func test_item_pickup():
 	var a = InputEventAction.new()
 	a.action = "pickup"
 	a.pressed = true
-	player._input(a)
+	_player._input(a)
 	
 	yield(get_tree().create_timer(1), "timeout")
 	assert_freed(item_drop)
