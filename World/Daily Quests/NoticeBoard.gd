e00416d1c79f6d5c203f77cde5488cc0f9c0217d
onepersonhere <51043502+onepersonhere@users.noreply.github.com>
2022-07-08 17:38:07 +0800

    parse quest successful
    left instantiating quest nodes

--- World/Daily Quests/NoticeBoard.gd
+++ World/Daily Quests/NoticeBoard.gd
@@ -1,22 +1,25 @@
 extends CanvasLayer
-var val = "stringValue"
+var val = "mapValue"
 
 onready var http = $HTTPRequest
 onready var http2 = $HTTPRequest2
 var at_check_quest_done = false;
 var quest_done = {
-"quest_1": {val : "Not Done"},
-"quest_2": {val : "Not Done"},
-"quest_3": {val : "Not Done"}
+"quest_1": {"stringValue" : "Not Done"},
+"quest_2": {"stringValue" : "Not Done"},
+"quest_3": {"stringValue" : "Not Done"}
 }
 onready var datetime = OS.get_datetime()
-onready var time = datetime["weekday"]
+onready var time = 5 # datetime["weekday"] for now use 5
 onready var user_id = str(datetime["day"]) + str(datetime["month"]) + str(datetime["year"]) + Firebase.user_info.id
 
 onready var text_field_1 = $Control/Col1/RichTextLabel
 onready var text_field_2 = $Control/Col2/RichTextLabel
 onready var text_field_3 = $Control/Col3/RichTextLabel
 
+var quest_node_1: Quest = null
+var quest_node_2: Quest = null
+var quest_node_3: Quest = null
 
 func _ready():
 	var link = "daily_quests/%s" % "day_" + str(time)
@@ -67,15 +70,15 @@ func set_quest_done(body):
 	var result_body = JSON.parse(body.get_string_from_ascii()).result.fields
 	
 	# manually set them
-	quest_done["quest_1"][val] = result_body["quest_1"][val]
-	quest_done["quest_2"][val] = result_body["quest_2"][val]
-	quest_done["quest_3"][val] = result_body["quest_3"][val]
+	quest_done["quest_1"]["stringValue"] = result_body["quest_1"]["stringValue"]
+	quest_done["quest_2"]["stringValue"] = result_body["quest_2"]["stringValue"]
+	quest_done["quest_3"]["stringValue"] = result_body["quest_3"]["stringValue"]
 	
-	if quest_done["quest_1"][val] == "Not Done":
+	if quest_done["quest_1"]["stringValue"] == "Not Done":
 		$Control/Col1/HBoxContainer/TextureButton1.disabled = false
-	if quest_done["quest_2"][val] == "Not Done":
+	if quest_done["quest_2"]["stringValue"] == "Not Done":
 		$Control/Col2/HBoxContainer/TextureButton2.disabled = false
-	if quest_done["quest_3"][val] == "Not Done":
+	if quest_done["quest_3"]["stringValue"] == "Not Done":
 		$Control/Col3/HBoxContainer/TextureButton3.disabled = false
 
 func on_quest_done(quest):
@@ -84,16 +87,39 @@ func on_quest_done(quest):
 
 func update_notice_board(body):
 	var result_body = JSON.parse(body.get_string_from_ascii()).result.fields
-	value_parser(text_field_1, result_body["quest_1"][val])
-	value_parser(text_field_2, result_body["quest_2"][val])
-	value_parser(text_field_3, result_body["quest_3"][val])
+	value_parser("quest_1", text_field_1, result_body["quest_1"][val].fields)
+	value_parser("quest_2", text_field_2, result_body["quest_2"][val].fields)
+	value_parser("quest_3", text_field_3, result_body["quest_3"][val].fields)
 	
-func value_parser(field, value):
-	field.text = value
-	# parses the value
-	# create a Quest Node
-	# rewards
-	# objectives
-	# instantiated when accepted
+func value_parser(quest_no, field, value):
+	var description = value["description"]["stringValue"]
+	var name = value["name"]["stringValue"]
+	var objective: Dictionary = value["objective"]["mapValue"]["fields"]
+	var rewards: Array = value["reward"]["arrayValue"]["values"]
+	var type = value["type"]["stringValue"]
+	
+	var text = name + "\n\n" + description + "\nObjective(s):\n\t"
+	
+	match type:
+		"QuestSlayObjective":
+			text += "amount" + ": " + objective["amount"]["integerValue"] + "\n\t"
+			text += "enemies" + ": " + objective["battler_to_slay"]["stringValue"] + "\n\t"
+			
+			text += "rewards:\n\t\t"
+			for reward in rewards:
+				var fields = reward.mapValue.fields
+				text += fields.amount.integerValue + " " + fields.item_name.stringValue + "\n\t\t"
+			 
+	field.text = text
+	
+	create_quest_node(quest_no, description, name, objective, rewards, type)
+		
+func create_quest_node(quest_no, description, name, objective, rewards, type):
+	print_debug("reached")
+# parses the value
+# create a Quest Node
+# rewards
+# objectives
+# instantiated when accepted
 
 
