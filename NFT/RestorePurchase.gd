8a0fed4b2bbc7bc8427e8518f461a3335adb26af
onepersonhere <51043502+onepersonhere@users.noreply.github.com>
2022-07-10 13:08:47 +0800

    Done

--- NFT/RestorePurchase.gd
+++ NFT/RestorePurchase.gd
@@ -7,6 +7,9 @@ onready var contract_addr = $ConfirmationDialog/VBoxContainer/contract_addr
 onready var notification = $AcceptDialog
 var base_url = "https://testnets-api.opensea.io/api/v1/asset/"
 var creator_addr = "0xd8b6eb581f77b1205fa312ec9c1a22d960b5900f"
+var item_name
+var img_req = false
+var img
 
 func _ready():
 	$ConfirmationDialog.popup_centered()
@@ -18,8 +21,10 @@ func _on_ConfirmationDialog_confirmed():
 
 
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
-	if response_code == 200:
+	if response_code == 200 && !img_req:
 		check(JSON.parse(body.get_string_from_ascii()).result)
+	elif response_code == 200 && img_req:
+		img = InventoryManager.image_loader(body, img_req, item_name)
 	else:
 		print_debug(response_code)
 
@@ -37,12 +42,13 @@ func check(result_body):
 		quantity = owner["quantity"]
 		var addr = owner["owner"]["address"]
 		
-		if addr == wallet_addr.text:
+		if addr == wallet_addr.text.to_lower():
 			found = true;
 			break;
 	
 	if found && created:
-		if add_item_to_inventory(quantity, result_body["traits"]):
+		if add_item_to_inventory(result_body["name"], result_body["description"],
+		 quantity, result_body["traits"], result_body["image_url"]):
 			notification.dialog_text = "item(s) added to inventory"
 		else:
 			notification.dialog_text = "item(s) already in inventory"
@@ -50,9 +56,12 @@ func check(result_body):
 		notification.dialog_text = "item does not exist"
 	notification.popup_centered()
 
-func add_item_to_inventory(quantity, traits):
-	# grab item pic
-	# parse item traits
-	# add them as JSON
-	# add them to inventory
-	return true
+func add_item_to_inventory(name, desc, quantity, traits, img_url):
+	# request image first
+	item_name = name
+	img_req = true
+	http.request(img_url)
+	yield(http, "request_completed")
+	img_req = false
+	
+	InventoryManager.add_new_item(name, quantity, InventoryManager.parse_traits(traits, desc), img)
