1206ca5bc3a1ab237daf9f1012cd08aaecc48359
Michael Kang <51043502+onepersonhere@users.noreply.github.com>
2022-07-22 07:29:19 +0800

    fixed shijun's bugs

--- Hitboxes and Hurtboxes/Hurtbox.gd
+++ Hitboxes and Hurtboxes/Hurtbox.gd
@@ -7,7 +7,7 @@ const HitEffect = preload("res://Effects/HitEffect.tscn")
 func _on_Hurtbox_area_entered(_area):
 	if show_hit:
 		var effect = HitEffect.instance()
-		var main  = get_node("/root/World")
+		var main = get_tree().get_nodes_in_group("map")[0]
 		main.add_child(effect)
 		effect.global_position = global_position
 
