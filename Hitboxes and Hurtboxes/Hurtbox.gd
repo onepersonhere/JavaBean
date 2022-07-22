623eee6496f776d6f068673e07a57d89658b05d6
Ng Shi Jun <shijunng2010@gmail.com>
2022-07-22 14:35:00 +0800

    bug fixes

--- Hitboxes and Hurtboxes/Hurtbox.gd
+++ Hitboxes and Hurtboxes/Hurtbox.gd
@@ -4,10 +4,9 @@ export(bool) var show_hit = true
 
 const HitEffect = preload("res://Effects/HitEffect.tscn")
 
-func _on_Hurtbox_area_entered(_area):
+func _on_HurtBox_area_entered(_area):
 	if show_hit:
 		var effect = HitEffect.instance()
 		var main = get_tree().get_nodes_in_group("map")[0]
 		main.add_child(effect)
 		effect.global_position = global_position
-
