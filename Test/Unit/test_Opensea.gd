4898dcbb24c4fcae6752d504e3705d29a986ef65
Michael Kang <51043502+onepersonhere@users.noreply.github.com>
2022-07-24 08:28:46 +0800

    added more test case

--- Test/Unit/test_Opensea.gd
+++ Test/Unit/test_Opensea.gd
@@ -1,10 +1,35 @@
 extends 'res://addons/gut/test.gd'
 
+var openseas = load("res://NFT/Openseas.tscn")
+var _openseas;
+var _UI;
+
 func before_each():
-	pass
-
+	_openseas = openseas.instance()
+	_UI = load("res://UI/UI.tscn").instance()
+	get_tree().get_root().add_child(_UI)
+	get_tree().get_root().add_child(_openseas)
+	
 func after_each():
-	pass
+	if weakref(_openseas).get_ref() != null:
+		_openseas.queue_free()
+	_UI.queue_free()
 
-func test_Opensea():
-	pass
+func test_load_assets():
+	# see if assets are loaded
+	var children = _openseas.get_node("Shop/Control/ScrollContainer/HBoxContainer/VBoxContainer/MarginContainer").get_children()
+	assert_not_null(children)
+
+func test_back():
+	_openseas.get_node("Shop")._on_Back_pressed()
+	yield(get_tree().create_timer(0.5), "timeout")
+	assert_false(is_instance_valid(_openseas))
+
+func test_item_appears_in_inventory():
+	_openseas.check_purchase(
+		"98022765745955927761920208486426559694950621028490842310820234511019612831754",
+		"0x88b48f654c30e99bc2e4a1559b4dcf1ad93fa656")
+	
+	yield(get_tree().create_timer(10), "timeout")
+	assert_not_null(PlayerInventory.inventory[3][0])
+
---
+++ Test/Unit/test_combat.gd
@@ -0,0 +1,47 @@
+extends 'res://addons/gut/test.gd'
+
+var world = load("res://World/World.tscn")
+var _world = null
+var player = load("res://Characters/MainCharacter.tscn")
+var _player = null
+var UI = load("res://UI/UI.tscn")
+var _UI = null
+
+func before_each():
+	_world = world.instance()
+	_UI = UI.instance()
+	_player = player.instance()
+	
+	get_node("/root").add_child(_UI)
+	PlayerStats.UI_created()
+	
+	get_node("/root").add_child(_world)
+	get_node("/root/World/YSort").add_child(_player)
+
+func after_each():
+	_world.queue_free()
+	_UI.queue_free()
+
+func test_receive_damage():
+	pass
+
+func test_give_damage():
+	pass
+
+func test_damage_changed():
+	pass
+
+func test_distance_to_receive_damage():
+	pass
+
+func test_distance_to_give_damage():
+	pass
+
+func test_death_screen():
+	pass
+
+func test_enemy_death():
+	pass
+
+func test_enemy_ai():
+	pass
