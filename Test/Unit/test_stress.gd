fbcd8dca295a227e37ca864e2ea1aad7b8241ac2
Michael Kang <51043502+onepersonhere@users.noreply.github.com>
2022-07-24 14:16:15 +0800

    Update test_stress.gd

--- Test/Unit/test_stress.gd
+++ Test/Unit/test_stress.gd
@@ -1,16 +1,49 @@
-extends Node
+extends 'res://addons/gut/test.gd'
 
+var world = load("res://World/World.tscn")
+var _world = null
+var player = load("res://Characters/MainCharacter.tscn")
+var _player = null
+var UI = load("res://UI/UI.tscn")
+var _UI = null
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func before_each():
+	_world = world.instance()
+	_UI = UI.instance()
+	_player = player.instance()
+	
+	get_node("/root").add_child(_UI)
+	PlayerStats.UI_created()
+	
+	get_node("/root").add_child(_world)
+	get_node("/root/World/YSort").add_child(_player)
 
+func after_each():
+	_world.queue_free()
+	_UI.queue_free()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# These tests should be run in isolation
+func test_stress_bat_100():
+	#var bat = load("res://Enemies/Bat.tscn")
+	
+	#for i in range(100):
+	#	var _bat = bat.instance()
+	#	_world.add_child(_bat)
+	#assert_almost_eq(_world.get_child_count(), 100, 10)
+	pass
+func test_stress_bat_300():
+	#var bat = load("res://Enemies/Bat.tscn")
+	
+	#for i in range(500):
+	#	var _bat = bat.instance()
+	#	_world.add_child(_bat)
+	#assert_almost_eq(_world.get_child_count(), 300, 10)
+	pass
+func test_stress_bat_500():
+	#var bat = load("res://Enemies/Bat.tscn")
+	
+	#for i in range(500):
+	#	var _bat = bat.instance()
+	#	_world.add_child(_bat)
+	#assert_almost_eq(_world.get_child_count(), 500, 10)
+	pass
